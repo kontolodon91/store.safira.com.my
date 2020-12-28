b3aeb1e1ea660c6bc6c18323e019ddfe16a4efc9
kontolodon91 <67394127+kontolodon91@users.noreply.github.com>
2020-12-28 17:13:03 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>Hacked By ./G1L4N6_ST86</title>
+</head>
+<BODY background="https://res.cloudinary.com/practicaldev/image/fetch/s--WRq-cBQj--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://steamuserimages-a.akamaihd.net/ugc/938320481074549559/B39B7EEF331845AD144B12F36E1E911B559BAEF2/">
+    
+    <center><br><br><br><br><br><br><br>
+        <img src="https://h.top4top.io/p_1805q683b1.jpg" width="200" height="200">
+        </center><br>
+        <center><b><style type="text/css">
+        #highlight{font:bold 50px Impact,Arial,Sans-Serif;}
+      </style>
+      <script language="javascript" type="text/javascript">
+           var teks="Hacked By ./G1L4N6_ST86" // teks Anda di sini
+           var speed=15 // atur kecepatan perubahan warna
+      
+      if (document.all||document.getElementById) {
+           document.write('<span id="highlight">' + teks + '</span>')
+           var storetext=document.getElementById? document.getElementById("highlight") :
+      document.all.highlight
+      } else document.write(text)
+      var hex=new Array("00","14","28","3C","50","64","78","8C","A0","B4","C8","DC","F0")
+      
+      var r=1
+      var g=1
+      var b=1
+      var seq=1
+      
+      function changetext() {
+           rainbow="#"+hex[r]+hex[g]+hex[b]
+           storetext.style.color=rainbow
+      }
+      
+      function change() {
+      if (seq==6) {
+      b--
+      if (b==0)
+      seq=1
+      }
+      if (seq==5) {
+      r++
+      if (r==12)
+      seq=6
+      }
+      if (seq==4) {
+      g--
+      if (g==0)
+      seq=5
+      }
+      if (seq==3) {
+      b++
+      if (b==12)
+      seq=4
+      }
+      if (seq==2) {
+      r--
+      if (r==0)
+      seq=3
+      }
+      if (seq==1) {
+      g++
+      if (g==12)
+      seq=2
+      }
+      changetext()
+      }
+      
+      function starteffect() {
+           if (document.all||document.getElementById)
+           flash=setInterval("change()",speed)
+      }
+      
+      starteffect()
+      </script></center>
+      <br><center>
+      <b><h1><font face="courier new" color="WHITE">Cukimay Cyber Team</font></h1></b>
+      <b><h3><font face="courier new" color="WHITE">Thanks To</font></h3></b>
+      <b><h3><font face="courier new" color="WHITE"><marquee direction="left">| XRealX Sec Team | Cukimay Cyber Team | Jawa Barat Cyber Team | </marquee></h3></font></b>
+      
+      <a href="https://wa.me/6283876910157?text=Assalamu%27alaikum%2C%20perkenalkan%20nama%20saya....." class="link-button">contact me</a>
+      <br></center>
+      <script>
+  function autoplay(){
+       var audio = document.getElementById("audio");
+       audio.autoplay();
+                 }
+   </script>
+
+<input type="button" value="gilangrian066@gmail.com" onclick="autoplay()">
+
+<center> 
+       <br> 
+       	       	<audio autoplay="autoplay" controls="asu" src="https://l.top4top.io/m_17285bith0.mp3" type="audio/mpeg"></audio>
+      </center> 
+    
+
+</BODY>
+</html>
